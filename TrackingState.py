7a31a76ba514c55514d96ff6894c49a6cb4a25ec
lissu yks <lissu1@users.noreply.github.com>
2015-08-29 21:34:12 +0300

    trackingstate.py

--- TrackingState.py
+++ TrackingState.py
@@ -1,12 +1,41 @@
 __author__ = 'lissu1'
 
-import string
+from string import ascii_lowercase, ascii_uppercase
 
-# print(string.ascii_lowercase)
-# print(string.ascii_uppercase)
-# print(string.punctuation)
-# print(string.whitespace)
+my_punctuation = '!?,. ;"\''
+input_string = '18,12312,171,763,98423,1208,216,11,500,18,241,0,32,20620,27,10'
 
-lowercase = string.ascii_lowercase
-for index in range(len(lowercase)):
-    print(index, lowercase[index])
+mode = 'UPPER'
+
+def main():
+    for ch in input_string.split(','):
+        decoder(ch)
+
+
+# mode = 'UPPER'
+def decoder(coded_ch):
+    global mode
+    if mode == 'UPPER':
+        reminder = int(coded_ch) % 27
+        if reminder != 0:
+            decoded = ascii_uppercase[reminder-1]
+            return print(decoded)
+        else:
+            mode = 'LOWER'
+    elif mode == 'LOWER':
+        reminder = int(coded_ch) % 27
+        if reminder != 0:
+            decoded = ascii_lowercase[reminder-1]
+            return print(decoded)
+        else:
+            mode = 'PUNCTUATION'
+    elif mode == 'PUNCTUATION':
+        reminder = int(coded_ch) % 9
+        if reminder != 0:
+            decoded = my_punctuation[reminder-1]
+            return print(decoded)
+        else:
+            mode = 'UPPER'
+
+
+main()
