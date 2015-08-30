a8c0aa4b8c358f43165db5a2ba0fb78ca1dc419d
lissu yks <lissu1@users.noreply.github.com>
2015-08-30 20:54:03 +0300

    hashtags update

--- hashtags.py
+++ hashtags.py
@@ -1,8 +1,29 @@
 __author__ = 'lissu1'
 
 
-i_range = range(4)
+i_range = range(8)
 
+print('diamond')
+   ##
+  ####
+ ######
+########
+ ######
+  ####
+   ##
+
+half_i_range = int(len(i_range)/2)
+for i in i_range:
+    if i < half_i_range:
+        hashtags = 2 * i + 2
+        spaces = half_i_range - i - 1
+        print(' ' * spaces, '#' * hashtags)
+    else:
+        hashtags = len(i_range)-2 * i
+        print(' ' * i, '#' * hashtags)
+
+print()
+print('diamond bottom')
 
 
 ########
@@ -10,12 +31,12 @@ i_range = range(4)
   ####
    ##
 
-for i in i_range:
-    hashtags = 2 * len(i_range)-2 * i
+for i in range(half_i_range):
+    hashtags = len(i_range)-2 * i
     print(' ' * i, '#' * hashtags)
 
-
 print()
+print('rectangle bottom to top')
 
 
 #####
@@ -29,7 +50,7 @@ for i in i_range:
     hashtags = len(i_range)-i
     print('#' * hashtags)
 
-print()
+print('rectangle sideways')
 
 
 
